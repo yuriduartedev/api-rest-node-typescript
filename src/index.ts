100011fb81a42591d9ab638a57f67bbcec1105d6
Yuri Duarte <yuriduartedev@gmail.com>
2024-01-23 12:44:10 -0400

    setup dotenv and tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { server } from "./server/Server";
 
-server.listen(3333, () => {
-  console.log("Server is listening on port 3333");
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
